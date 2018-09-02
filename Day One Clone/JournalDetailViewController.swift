a00c6780f5918e96f968a4cb587ee3941b633af6
jhollis67 <jhollis67@me.com>
2018-09-02 14:05:25 -0500

    Initial Commit

---
+++ Day One Clone/JournalDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JournalDetailViewController.swift
+//  Day One Clone
+//
+//  Created by Justin Hollis on 9/2/18.
+//  Copyright © 2018 Justin Hollis. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
